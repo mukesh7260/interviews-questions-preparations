e9bf1b7ce553e5daa7671f0605132b08c0ca8d84
mukesh7260 <mk2648054@gmail.com>
2025-02-21 08:18:45 +0000

    commit

--- try.py
+++ try.py
@@ -1,21 +1,6 @@
-# import re
-
-# s = input('inter mobile number : ')
-# f = re.match(r"(?:\+91|91|0)?[\s]?[7-9][0-9]{9}",s)
-
-# if f:   
-#     print('valid mobile number')
-# else:
-#     print('invalid mobile number')
-
-
-
-import re
-
-email = input("Enter email address: ")
-# regex = "^[A-Z0-9.%+-]+@[A-Z0-9]+\.[A-Z]{2,}$"
-regex = r"^[\w.%+-]+@[A-Za-z0-9]+\.[A-Za-z]{3,}$"
-if len(email) > 7 and re.match(regex, email, re.IGNORECASE) is not None:
-    print("Valid email address")
-else:
-    print("Invalid email address")
+l = [2,45,87,32,65,37]
+max_element = l[0]
+for i in l:
+    if i > max_element:
+        max_element = i
+print(max_element) 
