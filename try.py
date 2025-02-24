82795f4f20bd067644d6b4246d8434ec551d73cc
mukesh7260 <mk2648054@gmail.com>
2025-02-24 17:18:23 +0000

    commmit

--- try.py
+++ try.py
@@ -1,11 +1,8 @@
 
+n = int(input('enter no. of rows : ')) 
+for i in range(n+1):
+    print("  "*(n-i) + " *  "*i)
+for i in range(n-1,-1,-1):
+    print("  "*(n-i) + " *  "*i) 
 
-# sorting list eithor assending or desending
 
-l = [23,45,8,47,27,54] 
-for i in range(len(l)):
-    for j in range(len(l)):
-        if l[i] > l[j]:
-            l[i] , l[j] = l[j] , l[i] 
-
-print(l)
